63ea82eef11eddd7aafe17355a8147fff89f0b17
jakobowsky <jakob.wolitzki@gmail.com>
2020-03-14 20:12:06 +0100

    Created branch

--- movies/views.py
+++ movies/views.py
@@ -6,4 +6,4 @@ def home_page(request):
     user_query = str(request.GET.get('query', ''))
     search_result = Movie.objects.filter(name__icontains=user_query)
     stuff_for_frontend = {'search_result': search_result}
-    return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+    return render(request, 'movies/movies_stuff.html', stuff_for_frontend) 
