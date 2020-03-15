75a92c698156d325c0c7b03ae4ad9bd38461f6fa
jakobowsky <jakob.wolitzki@gmail.com>
2020-03-15 02:00:58 +0100

    added created

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_page, create
+from .views import home_page, create, edit
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
     path('create/', create, name='create'),
+    path('edit/<str:movie_id>', edit, name='edit'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -28,3 +28,23 @@ def create(request):
         except Exception as e:
             pass
     return redirect('/')
+
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'name': request.POST.get('name'),
+            'picture': request.POST.get('picture'),
+            'rating': int(request.POST.get('rating')),
+            'notes': request.POST.get('notes')
+        }
+        try:
+            movie_obj = Movie.objects.get(id=movie_id)
+            movie_obj.name = data.get('name')
+            movie_obj.picture = data.get('picture')
+            movie_obj.rating = data.get('rating')
+            movie_obj.notes = data.get('notes')
+            movie_obj.save()        
+        except Exception as e:
+            pass
+        return redirect('/')
